b43849ef4218fd55ed4319893522acadbff82b60
Fredrik Sundberg <fredrik@sundbergmedia.com>
2019-11-27 17:32:56 +0100

    Add method for answering questions

--- server.js
+++ server.js
@@ -1,29 +1,48 @@
 const express = require('express')
 const path = require('path')
+const bodyParser = require('body-parser')
 const cors = require('cors')
 const debug = require('debug')('app')
 const app = express()
 const _ = require('lodash')
 const config = { port: 4244 }
-const answers = require('./data/questions.json')
+const questions = require('./data/questions.json')
 
 // allow CORS
 app.use(cors())
+
+// handle POST requests
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 // serve static files
 app.use(express.static(path.join(__dirname, 'dist')))
 app.use(express.static(path.join(__dirname, 'src')))
 
 // output questions data
-app.get('/data', async (req, res, next) => {
-  // TODO: randomize answers, etc...
-  res.send(answers)
+app.get('/questions', async (req, res, next) => {
+  // TODO: randomize questions, etc...
+  res.send(questions)
+})
+
+// POST quiz results
+app.post('/answer', (req, res, next) => {
+  const { question, answer } = req.body
+  const questionExists = (question && questions[question])
+
+  if (!questionExists) return res.send('question dos not exist!')
+
+  const correctAnswer = questions[question].correct
+  const result = (correctAnswer === answer)
+  const message = (result) ? 'Currect answer!' : question + ' is not "' + answer + '"..!'
+  res.send(message)
 })
 
 // serve index.html to all other GET requests
 app.get('/*', (req, res, next) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'))
 })
+
 
 // start server
 app.listen(config.port, () => debug('Server runining @ port ' + config.port))
