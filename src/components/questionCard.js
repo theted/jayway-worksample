3ecb8a044fd49562fdb4992ff0227cdd5dd10768
Fredrik Sundberg <fredrik@sundbergmedia.com>
2019-11-28 18:28:16 +0100

    Handle answer state update

--- src/components/questionCard.js
+++ src/components/questionCard.js
@@ -1,21 +1,32 @@
-import React from "react"
+import React, { useState } from "react"
+
+const useForceUpdate = () => {
+  const [value, setValue] = useState(0)
+  return () => setValue(value => ++value)
+}
 
 const QuestionCard = props => {
   let { id, question, answers, sendAnswer } = props
+  const forceUpdate = useForceUpdate()
+  const [answ, setAnws] = useState([])
 
+  // submit answer to backend, handle response
   const handleSendAnswer = async event => {
     let answer = event.target.innerHTML
-    return sendAnswer(id, answer)
+
+    return sendAnswer(id, answer).then(result => {
+      answ[answer] = (result) ? 'correct' : 'incorrect'
+      // setAnws([...answ])
+      forceUpdate()
+    })
   }
 
   return (
     <div className="question">
       <h3>{question}</h3>
-
       {answers.map(answer =>
-        <div className="answer" onClick={handleSendAnswer} key={answer}>{answer}</div>
+        <div className={answ[answer] || 'answer'} onClick={handleSendAnswer} key={answer}>{answer}</div>
       )}
-
     </div>
   )
 }
