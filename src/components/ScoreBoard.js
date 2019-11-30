c85ceb29fe017aa68a919a5c04da0a57a2361f4a
Fredrik Sundberg <fredrik@sundbergmedia.com>
2019-11-30 22:00:46 +0100

    Fix percentage calculation

--- src/components/ScoreBoard.js
+++ src/components/ScoreBoard.js
@@ -2,11 +2,15 @@ import React from "react"
 
 const ScoreBoard = props => {
   const { answers, score } = props
+  const incorrectAnswers = answers - score
+  const percentageWin = (score) ? Math.round((score / answers) * 100) : '0'
 
   return (
     <div className="ScoreBoard">
       <p>{answers} answers</p>
       <p>{score} correct answers</p>
+      <p>{incorrectAnswers} incorrect answers</p>
+      <p>Percentage win: {percentageWin} %</p>
     </div>
   )
 }
