64b2bd65003f1c83eed30a59e4c360c83e32f287
Fredrik Sundberg <fredrik@sundbergmedia.com>
2019-12-01 21:46:20 +0100

    Add support for timer end callback

--- src/components/progresstimer.js
+++ src/components/progresstimer.js
@@ -3,9 +3,13 @@ import Progressbar from './progressbar.js'
 import * as Config from '../config.js'
 
 const ProgressTimer = props => {
-  const { progress } = props
+  const { progress, endCallback } = props
 
   let remainingPercent = (progress / Config.maxAnswerTime) * 100
+
+  if (remainingPercent <= 0) {
+    endCallback()
+  }
 
   return (
     <div className="progress-timer">
