a71029a787803216b88688b7043b85e8cada552b
Fredrik Sundberg <fredrik@sundbergmedia.com>
2019-12-03 00:04:18 +0100

    On the way to disabling undesired double-click behaviour on answer..!

--- src/components/questionCard.js
+++ src/components/questionCard.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react"
+import Button from './Button.js'
 
 const useForceUpdate = () => {
   const [value, setValue] = useState(0)
@@ -9,10 +10,22 @@ const QuestionCard = props => {
   let { id, question, answers, sendAnswer } = props
   const forceUpdate = useForceUpdate()
   const [answ, setAnws] = useState([])
+  const [isClicked, setClicked] = useState(false)
 
   // submit answer to backend, handle response
   const handleSendAnswer = async event => {
     let answer = event.target.innerHTML
+
+    // TODO: temporarily disable button?
+    console.log('CLICKY::', id, answer)
+
+    setClicked(true)
+
+    // ... bit hack:y..!
+    setTimeout(() => {
+      console.log('RESET click!')
+      setClicked(false)
+    }, 3000) // <- ! very hack:y!!
 
     return sendAnswer(id, answer).then(result => {
       answ[answer] = (result) ? 'answer correct' : 'answer incorrect'
@@ -25,7 +38,7 @@ const QuestionCard = props => {
     <div className="question animated zoomIn">
       <h3>{question}</h3>
       {answers.map(answer =>
-        <div className={answ[answer] || 'answer'} onClick={handleSendAnswer} key={answer}>{answer}</div>
+        <Button className={answ[answer] || 'answer'} onClick={handleSendAnswer} key={answer} text={answer} disabled={isClicked} />
       )}
     </div>
   )
