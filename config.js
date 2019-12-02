ee4ab5ca0dda7348b6761035ee08c286f394d26d
Fredrik Sundberg <fredrik@sundbergmedia.com>
2019-12-02 01:08:05 +0100

    Update config

---
+++ config.js
@@ -0,0 +1,6 @@
+const config = {
+  port: 4244,
+  maxQuestions: 10,
+}
+
+module.exports = config
--- src/config.js
+++ src/config.js
@@ -1,5 +1,11 @@
-const defaultEndpoint = 'http://localhost:4244/'
+const port = 4244
+const defaultEndpoint = 'http://localhost:' + port + '/'
 const environment = (window.location.hostname === 'localhost') ? 'development' : 'production'
 const endpoint = (environment === 'production') ? 'https://' + window.location.hostname + '/' : defaultEndpoint
 
-export default { endpoint }
+// game configuration
+const maxQuestions = 10
+const maxAnswerTime = 15 * 1000 // in milliseconds
+
+export { endpoint, maxQuestions, maxAnswerTime, port }
+export default { endpoint, maxQuestions, maxAnswerTime, port }
